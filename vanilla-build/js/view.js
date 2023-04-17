1fc58a6730184f67b1fc882333231c87a828525a
Matt Smith <msmithfl@icloud.com>
2023-04-17 18:38:55 -0400

    Break

--- vanilla-build/js/view.js
+++ vanilla-build/js/view.js
@@ -1,23 +1,32 @@
 export default class View {
+  // multiple namespaces used for organization and separation
   $ = {};
+  $$ = {};
 
   constructor() {
-    this.$.menu = document.querySelector('[data-id="menu"]');
-    this.$.menuBtn = document.querySelector('[data-id="menu-btn"]');
-    this.$.menuItems = document.querySelector('[data-id="menu-items"]');
-    this.$.resetBtn = document.querySelector('[data-id="reset-btn"]');
-    this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]');
-    this.$.squares = document.querySelectorAll('[data-id="square"]');
-    this.$.modal = document.querySelector('[data-id="modal"]');
-    this.$.modalText = document.querySelector('[data-id="modal-text"]');
-    this.$.modalBtn = document.querySelector('[data-id="modal-btn"]');
-    this.$.turn = document.querySelector('[data-id="turn"]');
+    // Single elements
+    this.$.menu = this.#qs('[data-id="menu"]');
+    this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+    this.$.menuItems = this.#qs('[data-id="menu-items"]');
+    this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+    this.$.modal = this.#qs('[data-id="modal"]');
+    this.$.modalText = this.#qs('[data-id="modal-text"]');
+    this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+    this.$.turn = this.#qs('[data-id="turn"]');
+
+    // Element lists
+    this.$$.squares = this.#qsAll('[data-id="square"]');
 
     // UI-only event listeners
     this.$.menuBtn.addEventListener("click", (event) => {
-      this.$.menuItems.classList.toggle("hidden");
+      this.#toggleMenu();
     });
   }
+
+  /**
+   * Register all the event listeners
+   */
 
   bindGameResetEvent(handler) {
     this.$.resetBtn.addEventListener("click", handler);
@@ -28,8 +37,40 @@ export default class View {
   }
 
   bindPlayerMoveEvent(handler) {
-    this.$.squares.forEach((sqaure) => {
+    this.$$.squares.forEach((sqaure) => {
       sqaure.addEventListener("click", handler);
     });
+  }
+
+  /**
+   * DOM helper methods
+   */
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuBtn.classList.toggle("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+
+    icon.classList.toggle("fa-chevron-down");
+    icon.classList.toggle("fa-chevron-up");
+  }
+
+  // private syntax #
+  #qs(selector, parent) {
+    const el = parent
+      ? parent.querySelector(selector)
+      : document.querySelector(selector);
+
+    if (!el) throw new Error("Could not find elements");
+
+    return el;
+  }
+
+  #qsAll(selector) {
+    const elList = document.querySelectorAll(selector);
+
+    if (!elList) throw new Error("Could not find elements");
+
+    return elList;
   }
 }
